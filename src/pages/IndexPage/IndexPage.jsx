0943e07a3c5ac282db82d7fca19c7550d8d7c922
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-10-15 17:45:48 -0700

    fixes bootstrap bug

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import NavBar from '../../components/NavBar/NavBar';
 import IndexListItem from '../../components/IndexListItem/IndexListItem';
 import './IndexPage.css';
 
 const RoutesIndexPage = (props) => {
     return(
         <div>
-            <NavBar
-                user={props.user}
-                handleLogout={props.handleLogout}
-            />
             <h1>Motorcycle Routes Index Page</h1>
             <p>list of all routes (pubic)</p>
             <hr></hr>
-            <div className='routeList container'>
+            <div>
                 {props.routes.map(route => 
                     <IndexListItem
                         route={route}
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,18 @@
 // import { PromiseProvider } from 'mongoose';
 import React from 'react';
-import NavBar from '../../components/NavBar/NavBar';
 import {Link} from 'react-router-dom';
 import './LandingPage.css';
 
-const LandingPage = (props) => {
+const LandingPage = () => {
     return (
-        <div className='LandingPage'>
-            <NavBar
-                user={props.user}
-                handleLogout={props.handleLogout}
-            />
+        <div>
             <h1>Motorcyle Routes Landing Page</h1>
-            <br></br>
-            <h3>Your journey starts here</h3>
+            <hr></hr>
+            <h3>Your Journey Starts Here</h3>
             <br></br>
             <Link className='btn btn-info' to='/login'>Login</Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link className='btn btn-info' to='/signup'>Signup</Link>
-            <hr></hr>           
+            <Link className='btn btn-info' to='/signup'>Signup</Link>           
         </div>
     );
 }
