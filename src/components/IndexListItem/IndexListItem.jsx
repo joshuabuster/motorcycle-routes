00b225be351d70d816c01b919f82f8e7e6a6d8da
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-10-18 18:23:42 -0700

    adds styling to cards

--- src/components/IndexListItem/IndexListItem.jsx
+++ src/components/IndexListItem/IndexListItem.jsx
@@ -12,14 +12,14 @@ function IndexListItem (props) {
                 <p><b>End Point:</b> {props.route.endPoint}</p>
                 <p><b>Difficulty:</b> {props.route.difficulty}</p>
                 <p><b>Rating:</b> {props.route.rating}</p>
-                <br></br>
+                <hr></hr>
                 {/* !user logged in ? show empty div : show buttons */}
                 {
                     props.user ? 
                         <div>
                             {/* route not in users queue ? show Add : show REMOVE*/}
                             <button className="btn btn-success" onClick={() => props.handleAddToQueue(props.route._id)} disabled={props.route.subscribedUsers.includes(props.user._id)}>Add To Your Queue</button>
-                            &nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;
                             <button className="btn btn-danger" onClick={() => props.handleRemoveFromQueue(props.route._id)}>Remove From Queue</button>
                         </div>
                     :
